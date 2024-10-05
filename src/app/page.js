75a8f92ba57a4dab9ad3defc522314f8d535a81e
Muhammad Rafai <rafaimhd123@gmail.com>
2024-10-06 01:40:13 +0700

    last

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
+import { useAuth, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
+const {isSignedIn} = useAuth()
+
+// if (!auth.isSignedIn) {
+//   // kick ke mana gitu
+// }
+
+// console.log(auth)
   return (
     <div>
       <h1>Dashboard</h1>
